aaadd749e196693aa37609d0bdc92b47dd711db5
Christos Zisopoulos <christos@42linesofcode.com>
2009-08-01 16:46:48 +0200

    Plugin post-install hook to copy remotable.js and remotable.rb into the application where we are installed.

--- install.rb
+++ install.rb
@@ -1,1 +1,11 @@
-# Install hook code here
+puts "Copying files..."
+post_install_files = {
+  "generators/remotable/templates/remotable.js" => 'public/javascripts/remotable.js',
+  'generators/remotable/templates/remotable.rb' => 'config/initializers/remotable.rb'
+}
+
+post_install_files.each do |src, dest|
+  puts "Copying #{src.split('/').last} to #{File.join(RAILS_ROOT, dest)}"
+  FileUtils.cp_r(File.join(File.dirname(__FILE__) , src), File.join(RAILS_ROOT, dest))
+end
+puts "Files copied - Installation complete!"

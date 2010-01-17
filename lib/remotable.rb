132163e993748287753639b0f22d3cde4b5fd087
Christos Zisopoulos <christos@42linesofcode.com>
2010-01-17 22:01:45 +0100

    Adding button_to support by way of duplicating the button_to code

--- lib/remotable.rb
+++ lib/remotable.rb
@@ -25,6 +25,42 @@ module ActionView
         link_to_without_remotable(*(args << options), &block)
       end
       alias_method_chain :link_to, :remotable
+      
+      def button_to(name, options = {}, html_options = {})
+        html_options = html_options.stringify_keys
+        convert_boolean_attributes!(html_options, %w( disabled ))
+
+        method_tag = ''
+        if (method = html_options.delete('method')) && %w{put delete}.include?(method.to_s)
+          method_tag = tag('input', :type => 'hidden', :name => '_method', :value => method.to_s)
+        end
+
+        form_method = method.to_s == 'get' ? 'get' : 'post'
+
+        request_token_tag = ''
+        if form_method == 'post' && protect_against_forgery?
+          request_token_tag = tag(:input, :type => "hidden", :name => request_forgery_protection_token.to_s, :value => form_authenticity_token)
+        end
+
+        if confirm = html_options.delete("confirm")
+          html_options["onclick"] = "return #{confirm_javascript_function(confirm)};"
+        end
+
+        url = options.is_a?(String) ? options : self.url_for(options)
+        name ||= url
+
+        html_options.merge!("type" => "submit", "value" => name)
+        
+        data_remote = html_options.delete('remote')
+        if (data_remote)
+          "<form method=\"#{form_method}\" action=\"#{escape_once url}\"" +
+          " data-remote=\"true\" data-method=\"#{method}\" class=\"button-to\"><div>" +
+          method_tag + tag("input", html_options) + request_token_tag + "</div></form>"
+        else
+          "<form method=\"#{form_method}\" action=\"#{escape_once url}\" class=\"button-to\"><div>" +
+          method_tag + tag("input", html_options) + request_token_tag + "</div></form>"
+        end        
+      end
     end
   end
 end
